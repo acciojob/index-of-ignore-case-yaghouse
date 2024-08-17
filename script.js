50ccb255aa5260e04237a25933085e713f68631e
YaGhouse <98881486+yaghouse@users.noreply.github.com>
2024-08-17 10:27:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-function indexOfIgnoreCase(s1, s2) {
-  // write your code here
-}
+function indexOfIgnoreCase(str, subStr) {
+    // convert both strings to lower case
+    var lowerStr = str.toLowerCase();
+    var lowerSubStr = subStr.toLowerCase();
 
+    // use indexOf to find the first occurrence of subStr in str
+    var index = lowerStr.indexOf(lowerSubStr);
+
+    // return the index
+    return index;
+}
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
